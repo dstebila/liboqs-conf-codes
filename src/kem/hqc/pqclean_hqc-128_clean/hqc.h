f62ef236040a0ad9316d08aba86a05d1790e3772
Douglas Stebila <dstebila@uwaterloo.ca>
2025-02-02 16:38:06 -0500

    Add confirmation code to HQC-128
    Signed-off-by: Douglas Stebila <dstebila@uwaterloo.ca>

--- src/kem/hqc/pqclean_hqc-128_clean/hqc.h
+++ src/kem/hqc/pqclean_hqc-128_clean/hqc.h
@@ -10,7 +10,7 @@
 
 void PQCLEAN_HQC128_CLEAN_hqc_pke_keygen(uint8_t *pk, uint8_t *sk);
 
-void PQCLEAN_HQC128_CLEAN_hqc_pke_encrypt(uint64_t *u, uint64_t *v, uint8_t *m, unsigned char *theta, const unsigned char *pk);
+void PQCLEAN_HQC128_CLEAN_hqc_pke_encrypt(uint64_t *u, uint64_t *v, uint64_t *cd, uint8_t *m, unsigned char *theta, const unsigned char *pk);
 
 uint8_t PQCLEAN_HQC128_CLEAN_hqc_pke_decrypt(uint8_t *m, uint8_t *sigma, const uint64_t *u, const uint64_t *v, const unsigned char *sk);
 
